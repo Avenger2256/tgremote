c43fc0a5ed378fac5cb51302b333248f8e56186a
Avenger2256 <146481813+Avenger2256@users.noreply.github.com>
2025-01-21 21:49:09 +0300

    main settings functionally

--- config.py
+++ config.py
@@ -6,17 +6,18 @@ def save(data):
         with open("settings.json", "w", encoding="utf-8") as f:
             json.dump(data, f, indent=4, ensure_ascii=False)
     except Exception:
-        pass
+        raise Exception
 
 def load():
     if not os.path.exists("settings.json"):
-        return "NoFile error"
+        raise NoFile
     try:
         with open("settings.json", "r", encoding="utf-8") as f:
             data = json.load(f)
         return data
     except (json.JSONDecodeError, Exception):
-        return "Decode/Other error"
+        raise OtherError
+
 
 def start():
 	cfg = load()
